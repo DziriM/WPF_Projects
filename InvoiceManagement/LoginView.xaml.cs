5891a6634435f3d423fc9d1bed166991d31fa137
mdziri <moujahed.dziri@live.fr>
2025-04-28 17:05:22 +0200

    DataBindings Added

---
+++ InvoiceManagement/LoginView.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace InvoiceManagement
+{
+    /// <summary>
+    /// Logique d'interaction pour LoginView.xaml
+    /// </summary>
+    public partial class LoginView : UserControl
+    {
+        public LoginView()
+        {
+            InitializeComponent();
+        }
+
+        public void OnLoginBtnClicked(object sender, RoutedEventArgs e)
+        {
+            string passwordEntered = PasswordBox.Password;
+
+            var envPw = Environment.GetEnvironmentVariable("InvoiceManagement");
+
+            if (envPw != null)
+            {
+                if (passwordEntered == envPw)
+                {
+                    // The correct password is : Test123
+
+                    //MessageBox.Show("Entered correct password !");
+
+                    // Transition vers la vue Profile
+                    // Rechercher le ContentControl principal (MainContent dans MainWindow)
+                    MainWindow parentWindow = Window.GetWindow(this) as MainWindow;
+                    if (parentWindow != null)
+                    {
+                        parentWindow.NavigateTo(new ProfileView());
+                    }
+                }
+                else
+                {
+
+                    MessageBox.Show("Login failed !");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Environnement variable not found !!!!");
+            }
+        }
+
+        public void OnPasswordChanged(object sender, EventArgs e)
+        {
+            LoginButton.IsEnabled = !string.IsNullOrEmpty(PasswordBox.Password);
+
+        }
+    }
+}
---
+++ InvoiceManagement/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace InvoiceManagement
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            NavigateTo(new LoginView());
+        }
+
+        public void NavigateTo(UserControl newView)
+        {
+            var fadeOut = new DoubleAnimation(1, 0, TimeSpan.FromMilliseconds(150));
+
+            fadeOut.Completed += (s, e) =>
+            {
+                MainContent.Content = newView;
+
+                var fadeIn = new DoubleAnimation(0, 1, TimeSpan.FromMilliseconds(150));
+
+                MainContent.BeginAnimation(OpacityProperty, fadeIn);
+            };
+
+            MainContent.BeginAnimation(OpacityProperty, fadeOut);
+
+        }
+    }
+}
